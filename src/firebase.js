465f8f9deae8780c3c9baa051e64a140ee0de749
William Chou <wtchou712@gmail.com>
2017-10-07 19:29:35 -0700

    signing up with firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDeoaen6u6iDFH-ssi9NQ-crjMAT1-R8es",
+  authDomain: "goalcoach-3d437.firebaseapp.com",
+  databaseURL: "https://goalcoach-3d437.firebaseio.com",
+  projectId: "goalcoach-3d437",
+  storageBucket: "",
+  messagingSenderId: "148774106208"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
